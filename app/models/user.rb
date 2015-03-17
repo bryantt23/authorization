41ae6b23aa9da184c58159903653205cfc67bb98
bryantt23 <bryantt23@gmail.com>
2015-03-17 04:08:42 -0700

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true, length: { maximum: 50 },
-                    uniqueness: true
+  validates :name,  presence: true, length: { maximum: 50 }
+                    # ,uniqueness: true
   validates :email, presence: true, length: { maximum: 255 }
 
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 1 }
 
 
 end

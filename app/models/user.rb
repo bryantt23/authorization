9597eda2b7167f6525aaae9b3daae08e64ee2105
bryantt23 <bryantt23@gmail.com>
2015-03-17 02:27:01 -0700

    model user ch 6

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  validates :name,  presence: true, length: { maximum: 50 },
+                    uniqueness: true
+  validates :email, presence: true, length: { maximum: 255 }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+
+end
